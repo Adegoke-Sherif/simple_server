e2d06422fc3fc359e716ec6dd73226ae655c94b5
Adegoke Sherif <shiffyadegoke@yahoo.com>
2024-04-16 14:51:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Adegoke Sherif')
 }) 
 
-server.listen(8000, null, null, () => {
+server.listen(8900, null, null, () => {
   console.log('server is running')
 })
